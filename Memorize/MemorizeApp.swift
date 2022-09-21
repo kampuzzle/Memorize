7e59c1531e701bda07e3788dce5204d5c144225b
kampuzzle <paulabremenkamp@gmail.com>
2022-09-21 10:22:35 -0300

    hello world

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Paula on 9/21/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
