8cd5d7746d21fdcdb2c8cb34580fde2f31ecf591
kampuzzle <paulabremenkamp@gmail.com>
2022-09-23 08:32:20 -0300

    cards with different emojis

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,14 +8,73 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["🚲","🚘","✈️","🛵"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack{
+            CardView(content: emojis[0])
+            CardView(content: emojis[1])
+            CardView(content: emojis[2])
+            CardView(content: emojis[3])
+        }
+        .padding(.horizontal)
+        .foregroundColor(.blue)
+
     }
 }
+
+struct CardView: View {
+    @State var isFaceUp: Bool = false
+    var content: String
+    
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+
+            if isFaceUp {
+                shape.foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+                Text(content)
+                    .font(.largeTitle)
+            } else {
+                shape
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
     }
 }
