e9c89a50a4ae534d1e72d0ef12c1c9763257ab5f
kampuzzle <paulabremenkamp@gmail.com>
2022-11-22 10:56:47 -0300

    removed logical code from view

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,14 +9,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    var vehiclesTheme = ["🚲","🚘","✈️","🛵","🚌","🚑","🚒","🏍","🚀","🚢", "🛴", "🚃","🚖", "🚁","🛸","🛶","🚤","🚂"]
-    var sweetTheme = ["🍰","🍧","🍫","🍬","🍭","🥞","🧇","🍡","🍨","🧁","🎂","🍮", "🍩","🍪","🥮","🥠"]
-    var animalsTheme = ["🦖","🐅","🐆","🦒","🦙","🐑", "🐕","🐈", "🐍","🦜","🦢","🐇"]
-    
-    
-    @State var theme = ["🦖","🐅","🐆","🦒","🦙","🐑", "🐕","🐈", "🐍","🦜","🦢","🐇"].shuffled()
-    
+    let viewModel: EmojiMemoryGame
+        
     var body: some View {
         VStack {
             Text("MEMORIZE!")
@@ -24,14 +18,14 @@ struct ContentView: View {
                 .bold()
             ScrollView {
                 LazyVGrid(columns: [GridItem(.adaptive(minimum: 75))]) {
-                    ForEach(theme[0..<theme.count], id: \.self) { emoji in
-                        CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
+                    ForEach(viewModel.cards) { card in
+                        CardView(card: card).aspectRatio(2/3, contentMode: .fit)
                     }
                 }
             }
-            HStack {
-                changeTheme
-            }
+//            HStack {
+//                changeTheme
+//            }
             .padding(.horizontal)
             .font(.largeTitle)
         }
@@ -39,60 +33,56 @@ struct ContentView: View {
         .foregroundColor(.red)
     }
     
-    var changeTheme: some View {
-        HStack {
-            Button {
-                theme = sweetTheme.shuffled()
-            } label: {
-                VStack {
-                    Image(systemName: "fork.knife.circle")
-                    Text("Food").font(.body)
-                }
-            }
-            Spacer()
-            Button {
-                theme = animalsTheme.shuffled()
-            } label: {
-                VStack {
-                    Image(systemName: "pawprint.circle")
-                    Text("Animals").font(.body)
-                }
-            }
-            Spacer()
-            Button {
-                theme = vehiclesTheme.shuffled()
-            } label: {
-                VStack {
-                    Image(systemName: "car")
-                    Text("Vehicles").font(.body)
-                }
-            }
-        }
-        .foregroundColor(.accentColor)
-    }
+//    var changeTheme: some View {
+//        HStack {
+//            Button {
+//                theme = sweetTheme.shuffled()
+//            } label: {
+//                VStack {
+//                    Image(systemName: "fork.knife.circle")
+//                    Text("Food").font(.body)
+//                }
+//            }
+//            Spacer()
+//            Button {
+//                theme = animalsTheme.shuffled()
+//            } label: {
+//                VStack {
+//                    Image(systemName: "pawprint.circle")
+//                    Text("Animals").font(.body)
+//                }
+//            }
+//            Spacer()
+//            Button {
+//                theme = vehiclesTheme.shuffled()
+//            } label: {
+//                VStack {
+//                    Image(systemName: "car")
+//                    Text("Vehicles").font(.body)
+//                }
+//            }
+//        }
+//        .foregroundColor(.accentColor)
+//    }
 
 }
 
 struct CardView: View {
-    @State var isFaceUp: Bool = true
-    var content: String
+    let card: MemoryGame<String>.Card
     
     var body: some View {
         ZStack {
             let shape = RoundedRectangle(cornerRadius: 20)
 
-            if isFaceUp {
+            if card.isFaceUp {
                 shape.foregroundColor(.white)
                 shape.strokeBorder(lineWidth: 3)
-                Text(content)
+                Text(card.content)
                     .font(.system(size: 60))
             } else {
-                shape
+                shape.fill()
             }
         }
-        .onTapGesture {
-            isFaceUp = !isFaceUp
-        }
     }
 }
 
@@ -108,11 +98,11 @@ struct CardView: View {
 
 
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-            .preferredColorScheme(.dark)
-        ContentView()
-            .preferredColorScheme(.light)
-    }
-}
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//            .preferredColorScheme(.dark)
+//        ContentView()
+//            .preferredColorScheme(.light)
+//    }
+//}
--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -20,14 +20,16 @@ struct MemoryGame<CardContent> {
         
         for pairIndex in 0..<numberOfPairs {
             let content = createCardContent(pairIndex)
-            cards.append(Card(content: content))
-            cards.append(Card(content: content))
+            cards.append(Card(content: content, id: pairIndex*2))
+            cards.append(Card(content: content, id: pairIndex*2+1))
         }
     }
     
-    struct Card {
-        var isFaceUp: Bool = false
+    struct Card: Identifiable {
+        var isFaceUp: Bool = true
         var isMatched: Bool = false
         var content: CardContent
+        
+        var id: Int
     }
 }
